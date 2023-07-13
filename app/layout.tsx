05455775693c548f976b8dba176d2a2cf0d05a43
qqdflq <133198344+qqdflq@users.noreply.github.com>
2023-07-13 10:28:30 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./styles/highlight.scss";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "XQ ChatGPT",
+  description: "ChatGPT Chat Bot for XQ.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -17,7 +17,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "XQ ChatGPT",
     statusBarStyle: "default",
   },
 };
